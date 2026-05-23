9cb019f3d542a0b890e5e060c4086fec244ba99a
Wallace Souza <wallacesouza.130903@gmail.com>
2026-05-22 23:16:10 -0300

    Ajuste de pacotes da equipe

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
+# Schema de Login
 class Login(BaseModel):
-    email: EmailStr
-    senha: str
+    email: EmailStr = Field(
+        ...,
+        description="Email cadastrado do usuário.",
+        example="joao@email.com"
+    )
+    senha: str = Field(
+        ...,
+        description="Senha cadastrada do usuário.",
+        example="123456"
+    )
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,24 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
+#Schema de Usuário
 class UserCreate(BaseModel):
-    nome: str
-    email: EmailStr
-    senha: str
-    perfil: str = "user"
+    nome: str = Field(
+        ...,
+        description="Nome completo do usuário.",
+        example="João Silva"
+    )
+    email: EmailStr = Field(
+        ...,
+        description="Email válido do usuário.",
+        example="joao@email.com"
+    )
+    senha: str = Field(
+        ...,
+        description="Senha do usuário.",
+        example="123456"
+    )
+    perfil: str = Field(
+        default="user",
+        description="Perfil do usuário (ex: admin, user).",
+        example="user"
+    )
